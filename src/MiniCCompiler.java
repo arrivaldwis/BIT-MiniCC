c242d8bf3767e9ca6362a0af3d051ac0b18520b7
Arrival Dwi Sentosa <arrivaldwisentosa@gmail.com>
2018-06-24 16:07:16 +0800

    first commit

---
+++ src/MiniCCompiler.java
@@ -0,0 +1,37 @@
+import bit.minicc.CodeGenerator;
+import bit.minicc.Parser;
+import bit.minicc.Preprocessor;
+import bit.minicc.Scanner;
+import bit.minicc.SemanticChecker;
+
+
+public class MiniCCompiler {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		if(args.length < 1){
+			System.out.println("Usage: test.c");
+			return;
+		}else{
+			System.out.println(args[0]);
+		}
+		
+		Preprocessor pp = new Preprocessor();
+		Scanner scanner = new Scanner();
+		Parser parser = new Parser();
+		SemanticChecker sc = new SemanticChecker();
+		CodeGenerator cg = new CodeGenerator();
+		
+		String code = pp.run(args[0]);
+		scanner.run(code);
+		parser.run();
+		sc.run();
+		cg.run();
+		
+	}
+
+}
---
+++ src/bit/minicc/input/test.c
@@ -0,0 +1,28 @@
+
+/* this is our test file*/
+
+int main(){
+	int i;
+        
+        //Deadline Next Tuesday (22
+        //first, d is not declare (find variables use but not declare?)
+        //a, b and i are use but not initialized (find variables declared but not used)
+        //tree has been declare but not used (find variables used but not initialized)
+        
+        //build a report which variable that not declare
+        //which variable should be initialized
+        
+        //Finish Final Exam solve this problem: a = b * 4 / 2 + c
+        
+	return d+4+2+1;
+}
+
+int foo(){
+    foo1();
+    return 0;
+}
+
+int foo1(){
+    return 1;
+}
+
